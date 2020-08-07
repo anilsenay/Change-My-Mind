3dcd2cd9579b4b02e530bcafa2dc0403ab639073
Anıl Şenay <anilsenay3@gmail.com>
2020-08-07 16:23:19 +0300

    tab navigation screens created

---
+++ src/screens/profile.screen.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Profile() {
+  return (
+    <View>
+      <Text></Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
