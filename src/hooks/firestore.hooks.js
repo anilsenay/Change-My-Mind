3632ff249a141dc01c7e4066f4bf27bc150415f4
Anıl Şenay <anilsenay3@gmail.com>
2020-08-20 23:51:41 +0300

    new firestore hooks added

--- src/hooks/firestore.hooks.js
+++ src/hooks/firestore.hooks.js
@@ -3,6 +3,10 @@ import React from "react";
 import * as firebase from "firebase";
 import "firebase/firestore";
 
+function getCurrentUserId() {
+  return firebase.auth().currentUser.uid;
+}
+
 async function registerUser(
   uid,
   username,
@@ -69,4 +73,96 @@ async function createRound(proponent, proponent_msg) {
   });
 }
 
-export { registerUser, createDebate, createRound };
+async function getUser(uid = getCurrentUserId()) {
+  return await firebase
+    .firestore()
+    .collection("Users")
+    .doc(uid)
+    .get()
+    .then((doc) => {
+      if (doc.exists) {
+        console.log("Document data:", doc.data());
+      } else {
+        // doc.data() will be undefined in this case
+        console.log("No such document!");
+      }
+    })
+    .catch(function (error) {
+      console.log("Error getting document:", error);
+    });
+}
+
+async function getDebate(uid) {
+  return await firebase
+    .firestore()
+    .collection("Debate")
+    .doc(uid)
+    .get()
+    .then((doc) => {
+      if (doc.exists) {
+        console.log("Document data:", doc.data());
+      } else {
+        // doc.data() will be undefined in this case
+        console.log("No such document!");
+      }
+    })
+    .catch(function (error) {
+      console.log("Error getting document:", error);
+    });
+}
+
+async function getRound(uid) {
+  return await firebase
+    .firestore()
+    .collection("Rounds")
+    .doc(uid)
+    .get()
+    .then((doc) => {
+      if (doc.exists) {
+        console.log("Document data:", doc.data());
+      } else {
+        // doc.data() will be undefined in this case
+        console.log("No such document!");
+      }
+    })
+    .catch(function (error) {
+      console.log("Error getting document:", error);
+    });
+}
+
+async function updateUser(uid = getCurrentUserId(), data) {
+  return await firebase
+    .firestore()
+    .collection("Users")
+    .doc(uid)
+    .update({ ...data });
+}
+
+async function updateDebate(uid, data) {
+  return await firebase
+    .firestore()
+    .collection("Debate")
+    .doc(uid)
+    .update({ ...data });
+}
+
+async function updateRound(uid, data) {
+  return await firebase
+    .firestore()
+    .collection("Rounds")
+    .doc(uid)
+    .update({ ...data });
+}
+
+export {
+  getCurrentUserId,
+  registerUser,
+  createDebate,
+  createRound,
+  getUser,
+  getDebate,
+  getRound,
+  updateUser,
+  updateDebate,
+  updateRound,
+};
