ff2e7e4e5b0ea4eede29369d3d2ffd28b56f5bcc
Anıl Şenay <anilsenay3@gmail.com>
2020-09-02 23:59:50 +0300

    Add headers to explore and notification screens

--- src/screens/explore.screen.js
+++ src/screens/explore.screen.js
@@ -1,12 +1,24 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import Header from "../components/header";
+import SearchIcon from "../components/icons/search";
 
 export default function Explore() {
   return (
-    <View>
-      <Text>Explore</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Header
+        title="Explore"
+        titleAlignment="left"
+        rightIcon={<SearchIcon width={24} height={24} fill="black" />}
+      />
+    </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "white",
+  },
+});
--- src/screens/notification.screen.js
+++ src/screens/notification.screen.js
@@ -1,12 +1,41 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+import Header from "../components/header";
+import { Colors } from "../consts/colors";
 
 export default function Notifications() {
   return (
-    <View>
-      <Text></Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Header
+        title="Notifications"
+        titleAlignment="left"
+        rightIcon={
+          <View style={styles.markAllContainer}>
+            <Text style={styles.markAllText}>Mark all as read</Text>
+          </View>
+        }
+      />
+    </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "white",
+  },
+  markAllContainer: {
+    alignItems: "center",
+    height: "100%",
+    justifyContent: "center",
+  },
+  markAllText: {
+    width: 150,
+    padding: 8,
+    textAlign: "right",
+    fontWeight: "bold",
+    color: Colors.purple,
+  },
+});
